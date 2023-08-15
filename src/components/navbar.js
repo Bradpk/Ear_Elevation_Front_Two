b13787532fe4771b217afbb6c130d491ec55d5d7
brad <brad.pk@live.com>
2023-08-15 19:24:04 +0000

    g

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from "react";
+import styles from './Navbar.module.css'; // Import the CSS module
+
+const Navbar = () => {
+  return (
+    <nav className={styles.Navbar}> 
+      <ul>
+        <li>
+          <Link href="/dashboard">Home</Link>
+        </li>
+        <li>
+          <Link href="/interval">Interval</Link>
+        </li>
+        <li>
+          <Link href="/scale">Scale</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
