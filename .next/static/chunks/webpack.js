4304730c21c3eaf6f86e9d29f436b829213443b2
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-15 21:47:15 -0400

    g

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ea68f3dbb68fbef2"; }
+/******/ 		__webpack_require__.h = function() { return "8fca89480a71fc86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/scale.js
+++ src/pages/scale.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import * as Tone from 'tone';
 import Navbar from "../components/navbar";
+import styles from './scale.module.css';
 
 const ScaleGenerator = () => {
     const [generatedScale, setGeneratedScale] = useState('');
@@ -9,7 +10,7 @@ const ScaleGenerator = () => {
     const [attemptedQuestions, setAttemptedQuestions] = useState(0);
     const [correctAnswers, setCorrectAnswers] = useState(0);
 
-    const scales = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aolian', 'Locrian',];
+    const scales = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aolian', 'Locrian', 'Blues'];
 
     const generateRandomScale = () => {
         let randomScale = previousScale;
@@ -20,13 +21,13 @@ const ScaleGenerator = () => {
         setGeneratedScale(randomScale);
         setPreviousScale(randomScale);
         playScale(randomScale);
-    };  
+    };
 
-        const playScale = (interval) => {
-            const synth = new Tone.Synth().toDestination();
-            const now = Tone.now();
-    
-            switch (interval) {
+    const playScale = (interval) => {
+        const synth = new Tone.Synth().toDestination();
+        const now = Tone.now();
+
+        switch (interval) {
             case 'Ionian':
                 synth.triggerAttackRelease("C4", "4n", now);
                 synth.triggerAttackRelease("D4", "4n", now + 0.5);
@@ -97,40 +98,64 @@ const ScaleGenerator = () => {
                 synth.triggerAttackRelease("A#4", "4n", now + 3);
                 synth.triggerAttackRelease("C5", "4n", now + 3.5);
                 break;
+            case 'Blues':
+                synth.triggerAttackRelease("C4", "4n", now);
+                synth.triggerAttackRelease("D#4", "4n", now + 0.5);
+                synth.triggerAttackRelease("F4", "4n", now + 1);
+                synth.triggerAttackRelease("F#4", "4n", now + 1.5);
+                synth.triggerAttackRelease("G4", "4n", now + 2);
+                synth.triggerAttackRelease("A#4", "4n", now + 2.5);
+                synth.triggerAttackRelease("C4", "4n", now + 3);
+                break;
             default:
                 break;
         }
     };
 
-    const handleScaleSelection = (scale) => {
+    const handleScaleSelection = (scale, index) => {
         setSelectedScale(scale);
-        setAttemptedQuestions(attemptedQuestions + 1); 
+        setAttemptedQuestions(attemptedQuestions + 1);
 
         if (scale === generatedScale) {
             setCorrectAnswers(correctAnswers + 1);
             generateRandomScale();
+            document.getElementById(`scaleButton-${index}`).classList.add(styles.correct);
+            setTimeout(() => {
+                document.getElementById(`scaleButton-${index}`).classList.remove(styles.correct);
+            }, 400);
+        } else {
+            document.getElementById(`scaleButton-${index}`).classList.add(styles.incorrect);
+            setTimeout(() => {
+                document.getElementById(`scaleButton-${index}`).classList.remove(styles.incorrect);
+            }, 400);
         }
     };
 
     const scaleButtons = scales.map((scale, index) => (
-        <button key={index} onClick={() => handleScaleSelection(scale)}>
+        <button
+            key={index}
+            id={`scaleButton-${index}`}
+            onClick={() => handleScaleSelection(scale, index)}
+            className={styles.scaleButton}
+        >
             {scale}
         </button>
     ));
 
     return (
         <div>
-               <Navbar />
-            <h2>Scale Generator</h2>
-            <button onClick={generateRandomScale}>Generate Random Scale</button>
-            <button onClick={() => playScale(generatedScale)}>Replay Scale</button>
-            <p>Generated Scale: {generatedScale}</p>
-            <div>
-                {scaleButtons}
+            <Navbar />
+            <div className={styles.container}>
+                <h2 className={styles.heading}>Scale Generator</h2>
+                <button onClick={generateRandomScale} className={styles.button}>Generate Random Scale</button>
+                <div className={styles.space}></div>
+                <button onClick={() => playScale(generatedScale)} className={styles.button}>Replay Scale</button>
+                <div className={styles.buttonGrid}>
+                    {scaleButtons}
+                </div>
+                <p className={styles.stats}>Attempted: {attemptedQuestions} | Correct: {correctAnswers}</p>
             </div>
-            <p>Attempted: {attemptedQuestions} | Correct: {correctAnswers}</p>
         </div>
     );
 };
-
 export default ScaleGenerator;
