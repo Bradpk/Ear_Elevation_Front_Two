f1302a61ddc048311006f185fe654fe549b33356
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-17 18:29:21 -0400

    g

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,15 +1,58 @@
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
-import { useGlobalState } from '../../context/GlobalState';
-import authService from '../../services/auth.service';
+import React, { useState } from 'react';
 import Navbar from '../../components/navbar';
+import authService from '../../services/auth.service';
 import jwtDecode from 'jwt-decode';
 import Link from 'next/link';
+import axios from 'axios'; // Import axios for making HTTP requests
 
 const ProfilePage = () => {
+  const [postData, setPostData] = useState({ title: '', content: '' });
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('/api/posts', postData); // Replace with your API endpoint
+      console.log('Post request response:', response.data);
+    } catch (error) {
+      console.error('Error making post request:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPostData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <Navbar />
+      <h2>Create a New Post</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={postData.title}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="content">Content</label>
+          <textarea
+            id="content"
+            name="content"
+            value={postData.content}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit">Create Post</button>
+      </form>
     </div>
   );
 };
