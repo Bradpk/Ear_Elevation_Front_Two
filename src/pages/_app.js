e27b35930104d963e4f173e7a14a663f5d8659b2
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-08-22 16:12:29 -0400

    g

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,19 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { GlobalStateProvider } from '../context/GlobalState';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Function to retrieve user data from local storage
-    const getUserFromLocalStorage = () => {
-      const userData = localStorage.getItem('user');
-      if (userData) {
-        const user = jwtDecode(userData);
-        console.log('User data:', user);
-        /* dispatch to setGlobalState to user */
-        /* if state.user.user_id != null then we know a user is logged in */
-      }
-    };
-    getUserFromLocalStorage();
-  }, []);
   return (
     <GlobalStateProvider>
       <div className={styles.myGlobalStyles}><Component {...pageProps} /></div>
